71e7a6f2e247678b1e291c094c1ff5294bbb6362
alex <alexgaozhongze@gmail.com>
2019-09-23 11:33:19 +0800

    go beyond

--- applications/http/views/index/index.php
+++ applications/http/views/index/index.php
@@ -1,22 +1,35 @@
 <?php
 $this->title = 'go beyond';
 ?>
+<head>
+<style type='text/css'>
+body {
+  text-align: center;
+}
 
-<table>
+td {
+  margin: 100px;
+  overflow: auto;
+}
+
+</style>
+</head>
+
+<table width='88%' style="margin-left:10%">
     <tr>
-        <td style="padding:8px 18px;">code</td>
-        <td style="padding:8px 18px;">avg_cjs</td>
-        <td style="padding:8px 18px;">up</td>
-        <td style="padding:8px 18px;">cjs</td>
-        <td style="padding:8px 18px;">pre</td>
+        <td>code</td>
+        <td>avg_cjs</td>
+        <td>up</td>
+        <td>cjs</td>
+        <td>pre</td>
     </tr>
     <?php foreach($list as $value): ?>
     <tr>
-        <td style="padding:8px 18px;"><?= $value['code'] ?></td>
-        <td style="padding:8px 18px;"><?= $value['avg_cjs'] ?></td>
-        <td style="padding:8px 18px;"><?= $value['up'] ?></td>
-        <td style="padding:8px 18px;"><?= $value['cjs'] ?></td>
-        <td style="padding:8px 18px;"><?= $value['pre'] ?></td>
+        <td><?= $value['code'] ?></td>
+        <td><?= $value['avg_cjs'] ?></td>
+        <td><?= $value['up'] ?></td>
+        <td><?= $value['cjs'] ?></td>
+        <td><?= $value['pre'] ?></td>
     </tr>
     <?php endforeach; ?>
 </table>
