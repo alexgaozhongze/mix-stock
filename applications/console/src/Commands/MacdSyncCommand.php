ef08f46e3510a7668c559a02558d38f1725898fc
alex <alexgaozhongze@gmail.com>
2019-12-06 15:25:36 +0800

    go beyond

--- applications/console/src/Commands/MacdSyncCommand.php
+++ applications/console/src/Commands/MacdSyncCommand.php
@@ -24,18 +24,23 @@ class MacdSyncCommand
             $code = Flag::string('code');
 
             $connection=app()->dbPool->getConnection();
-            $sql = "SELECT `code` FROM `macd`";
-            $code && $sql .= " WHERE `code`=$code";
+            $sql = "SELECT `code`,`type` FROM `macd` WHERE `time`>=CURDATE()";
+            $code && $sql .= " AND `code`=$code";
             $sql .= " GROUP BY `code`";
             $code_list = $connection->createCommand($sql)->queryAll();
 
+            $code_type_list = array_column($code_list, 'type', 'code');
             $code_list = array_column($code_list, 'code');
 
             foreach ($code_list as $value) {
-                $sql = "SELECT `sp`,`time` FROM `macd` WHERE `code`=$value";
+                $sql = "SELECT `sp`,`time` FROM `macd` WHERE `code`=$value AND `type`=$code_type_list[$value] AND `time`>=CURDATE()";
                 $list = $connection->createCommand($sql)->queryAll();
 
-                $pre_l_value = [];
+                $min_time = reset($list)['time'];
+                $sql = "SELECT `ema12`,`ema26`,`dea` FROM `macd` WHERE `code`=$value AND `type`=$code_type_list[$value] AND `time`<'$min_time' ORDER BY `time` DESC";
+                $info = $connection->createCommand($sql)->queryOne();
+
+                $pre_l_value = $info ?: [];
                 foreach ($list as $l_value) {
                     $sp = $l_value['sp'];
 
@@ -72,60 +77,6 @@ class MacdSyncCommand
             }
 
         });
-    }
-
-    public function handle()
-    {
-        $connection=app()->dbPool->getConnection();
-
-        $table_name = 'hq_' . date('Ymd');
-        $sql = "SELECT `code`,`type` FROM `hsab` AS `a` WHERE `date`=CURDATE() AND `price` IS NOT NULL GROUP BY `code`";
-        $codes = $connection->createCommand($sql)->queryAll();
-
-        list($microstamp, $timestamp) = explode(' ', microtime());
-        $timestamp = "$timestamp" . intval($microstamp * 1000);
-
-        $urls = [];
-        array_walk($codes, function($item) use (&$urls, $timestamp) {
-            $key = str_pad($item['code'], 6, "0", STR_PAD_LEFT) . $item['type'];
-            $urls[] = "http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?token=4f1862fc3b5e77c150a2b985b12db0fd&rtntype=6&id=$key&type=m5k&authorityType=&js={%22data%22:(x)}";
-        });
-
-        QueryList::multiGet($urls)
-            ->concurrency(8)
-            ->withOptions([
-                'timeout' => 3
-            ])
-            ->success(function(QueryList $ql, Response $response, $index) use ($connection, $table_name) {
-                $response_json = $ql->getHtml();
-
-                $item = json_decode($response_json, true);
-                $data = $item['data']['data'] ?? [];
-                $info = $item['data']['info'] ?? [];
-
-                $sql_fields = "INSERT IGNORE INTO `macd` (`code`, `kp`, `sp`, `zg`, `zd`, `cjl`, `cje`, `zf`, `time`, `type`) VALUES ";
-                $sql_values = "";
-    
-                $code = $item['data']['code'];
-                $type = $info['mk'];
-    
-                array_walk($data, function($iitem) use (&$sql_values, $code, $type) {
-                    list($time, $kp, $sp, $zg, $zd, $cjl, $cje, $zf) = explode(',', $iitem);
-                    $zf = floatval($zf);
-                    $sql_values && $sql_values .= ',';
-
-                    $sql_values .= "($code, $kp, $sp, $zg, $zd, $cjl, $cje, $zf, '$time', $type)";
-                });
-
-                if ($sql_values) {
-                    $sql = $sql_fields . $sql_values;
-                    $connection->createCommand($sql)->execute();
-                }
-            })->error(function (QueryList $ql, $reason, $index){
-                // ...
-            })->send();
-
-        app()->dbPool->getConnection()->release();
     }
 
 }
