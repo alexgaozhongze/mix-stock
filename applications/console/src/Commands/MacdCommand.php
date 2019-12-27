e701386110f408e0d7d29cb1148aae2c71f030b4
alex <alexgaozhongze@gmail.com>
2019-12-27 14:06:52 +0800

    go beyond

--- applications/console/src/Commands/MacdCommand.php
+++ applications/console/src/Commands/MacdCommand.php
@@ -54,7 +54,7 @@ class MacdCommand
                 $data = $item['data']['data'] ?? [];
                 $info = $item['data']['info'] ?? [];
 
-                $sql_fields = "INSERT IGNORE INTO `macd` (`code`, `kp`, `sp`, `zg`, `zd`, `cjl`, `cje`, `zf`, `time`, `type`, `dif`, `dea`, `macd`, `ema12`, `ema26`) VALUES ";
+                $sql_fields = "INSERT IGNORE INTO `macd` (`code`, `kp`, `sp`, `zg`, `zd`, `cjl`, `cje`, `zf`, `time`, `type`, `dif`, `dea`, `macd`, `ema12`, `ema26` ,`ema5`, `ema10`, `ema20`, `ema60`) VALUES ";
                 $sql_values = "";
     
                 $code = $item['data']['code'];
@@ -71,23 +71,35 @@ class MacdCommand
                         if ($pre_l_value) {
                             $pre_ema12 = $pre_l_value['ema12'];
                             $pre_ema26 = $pre_l_value['ema26'];
+                            $pre_ema5 = $pre_l_value['ema5'];
+                            $pre_ema10 = $pre_l_value['ema10'];
+                            $pre_ema20 = $pre_l_value['ema20'];
+                            $pre_ema60 = $pre_l_value['ema60'];
                             $pre_dea = $pre_l_value['dea'];
                         } else {
-                            $sql = "SELECT `ema12`,`ema26`,`dea` FROM `macd` WHERE `code`=$code AND `type`=$type AND `time`<'$time' ORDER BY `time` DESC";
+                            $sql = "SELECT `ema12`,`ema26`,`ema5`,`ema10`,`ema20`,`ema60`,`dea` FROM `macd` WHERE `code`=$code AND `type`=$type AND `time`<'$time' ORDER BY `time` DESC";
                             $info = $connection->createCommand($sql)->queryOne();
 
                             if ($info) {
                                 $pre_ema12 = $info['ema12'];
                                 $pre_ema26 = $info['ema26'];
+                                $pre_ema5 = $info['ema5'];
+                                $pre_ema10 = $info['ema10'];
+                                $pre_ema20 = $info['ema20'];
+                                $pre_ema60 = $info['ema60'];
                                 $pre_dea = $info['dea'];
                             } else {
-                                $pre_ema12 = $pre_ema26 = $sp;
+                                $pre_ema12 = $pre_ema26 = $pre_ema5 = $pre_ema10 = $pre_ema20 = $pre_ema60 = $sp;
                                 $pre_dea = 0;
                             }
                         }
 
                         $ema12 = 2 / (12 + 1) * $sp + (12 - 1) / (12 + 1) * $pre_ema12;
                         $ema26 = 2 / (26 + 1) * $sp + (26 - 1) / (26 + 1) * $pre_ema26;
+                        $ema5 = 2 / (5 + 1) * $sp + (5 - 1) / (5 + 1) * $pre_ema5;
+                        $ema10 = 2 / (10 + 1) * $sp + (10 - 1) / (10 + 1) * $pre_ema10;
+                        $ema20 = 2 / (20 + 1) * $sp + (20 - 1) / (20 + 1) * $pre_ema20;
+                        $ema60 = 2 / (60 + 1) * $sp + (60 - 1) / (60 + 1) * $pre_ema60;
     
                         $dif = $ema12 - $ema26;
                         $dea = 2 / (9 + 1) * $dif + (9 - 1) / (9 + 1) * $pre_dea;
@@ -98,10 +110,14 @@ class MacdCommand
                         $l_value['macd'] = round($macd, 3);
                         $l_value['ema12'] = round($ema12, 3);
                         $l_value['ema26'] = round($ema26, 3);
+                        $l_value['ema5'] = round($ema5, 3);
+                        $l_value['ema10'] = round($ema10, 3);
+                        $l_value['ema20'] = round($ema20, 3);
+                        $l_value['ema60'] = round($ema60, 3);
 
                         $pre_l_value = $l_value;
 
-                        $sql_values .= "($code, $kp, $sp, $zg, $zd, $cjl, $cje, $zf, '$time', $type, $l_value[dif], $l_value[dea], $l_value[macd], $l_value[ema12], $l_value[ema26])";
+                        $sql_values .= "($code, $kp, $sp, $zg, $zd, $cjl, $cje, $zf, '$time', $type, $l_value[dif], $l_value[dea], $l_value[macd], $l_value[ema12], $l_value[ema26], $l_value[ema5], $l_value[ema10], $l_value[ema20], $l_value[ema60])";
                     }
                 });
 
