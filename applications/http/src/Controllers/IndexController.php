8fa8a448b8088f5dd5fedf7651b73e7c6bec655a
alex <alexgaozhongze@gmail.com>
2020-03-09 16:15:10 +0800

    go beyond

--- applications/http/src/Controllers/IndexController.php
+++ applications/http/src/Controllers/IndexController.php
@@ -52,10 +52,8 @@ class IndexController
         $urls = [];
         foreach ($list as $key => $value) {
             if ($key >= $index && $key < $index_end) {
-                $type = 1 == $value['type'] ? 'sh' : 'sz';
                 $market = 1 == $value['type'] ? 1 : 2;
                 $code = str_pad($value['code'], 6, '0', STR_PAD_LEFT);
-                //$urls[] = "http://quote.eastmoney.com/concept/$type$code.html#fschart-m5k";
                 $urls[] = "http://quote.eastmoney.com/basic/h5chart-iframe.html?code=$code&market=$market&type=m5k";
             }
         }
