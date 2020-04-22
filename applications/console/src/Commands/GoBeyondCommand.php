3711a7d2edc18b5062255a8953c9d6da83df5195
alex <alexgaozhongze@gmail.com>
2020-04-22 16:27:15 +0800

    go beyond

--- applications/console/src/Commands/GoBeyondCommand.php
+++ applications/console/src/Commands/GoBeyondCommand.php
@@ -50,6 +50,26 @@ class GoBeyondCommand
 
             $index = $index_end + 1;
             $redis->setex('index', 88888, $index);
+
+            $sql = "select code from date_code where date>='$dates[0]' order by date desc limit 1";
+            $info = $connection->createCommand($sql)->queryOne();
+            $code_str = $info['code'];
+
+            $sql = "select code,count(*) count from macd where time>='$dates[0]' and code in ($code_str) and ema5>ema60 and ema10>ema60 and ema20>ema60 group by code";
+            $list = $connection->createCommand($sql)->queryAll();
+            
+            $codes = array_column($list, 'code', 'count');
+            $times = array_column($list, 'count');
+            $max_times = max($times);
+            $code = $codes[$max_times];
+
+            $sql = "select code,type from hsab where code=$code and date=curdate()";
+            $info = $connection->createCommand($sql)->queryOne();
+
+            $market = 1 == $info['type'] ? 1 : 2;
+            $code = str_pad($info['code'], 6, '0', STR_PAD_LEFT);
+            $url = "http://quote.eastmoney.com/basic/h5chart-iframe.html?code=$code&market=$market&type=m5k";
+            exec("google-chrome '$url'");
         });
     }
 
